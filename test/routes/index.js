3a6cbaca1cd4f0d55d0a6e2b5ac5ab7706c2e878
zhangzongxiang <zhangzongxiang@yihecloud.com>
2017-01-22 17:42:45 +0800

    header css

--- test/routes/index.js
+++ test/routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Index' });
+    res.render('index', { title: '拾光镇-Memory Minecraft Server' });
+});
+
+router.get('/gallery', function(req, res, next) {
+    res.render('gallery', { title: '拾光镇画廊-Memory Minecraft Server' });	
 });
 
 module.exports = router;
