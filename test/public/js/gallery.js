aecd40323d12ab0f169b986500183ed6ccf0b421
zhangzongxiang <zhangzongxiang@yihecloud.com>
2017-01-25 16:22:04 +0800

    gallery style

--- test/public/js/gallery.js
+++ test/public/js/gallery.js
@@ -4,13 +4,27 @@ $(function(){
     gallery.css("height", $(window).height() - 50);
     $(window).resize(function () {
         gallery.css("height", $(window).height() - 50);
+        galleryInit();
     });
 
+    galleryInit();
+
+    $('#gallery-div dl').mousedown(function(e){
+        $('#gallery-div dl').css("z-index", "0");
+        $(this).css("z-index","1");
+    });
+
+    $("#gallery-modal").modal();
+});
+
+function galleryInit(){
     $('#gallery-div dl').each(function(){
         $(this).dragging({
             move : 'both',
-            randomPosition : true ,
+            randomPosition : true,
             hander:'.hander'
         });
+        var deg = 30 * Math.random() - 15;
+        $(this).css("transform","rotate(" + deg + "deg)");
     });
-});
+}
